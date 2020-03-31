b0cb0833b78d42328eb9ec0c4ddc7aa75108ccc2
grafcheg <dergraff@gmail.com>
2020-03-31 17:02:32 +1000

    promisify rework

--- src.js
+++ src.js
@@ -1,57 +1,53 @@
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+const mkdirPromisify = util.promisify(fs.mkdir);
+const linkPromisify = util.promisify(fs.link);
+const readdirPromisify = util.promisify(fs.readdir);
 
 // default folders name
 const fromDir = process.argv[2] || 'not_sorted';
 const toDir = process.argv[3] || 'sorted';
 
-const deleteArg = process.argv[4];
+const readDir = (base, level) => {
+  readdirPromisify(base)
+    .then(files => {
+      files.forEach(item => {
+        const localBase = path.join(base, item);
+        const state = fs.statSync(localBase);
 
-if (!fs.existsSync(fromDir)) {
-  console.error('Source folder does not exist');
-  process.exit(9);
-}
-
-fs.mkdir(toDir, { recursive: true }, (err) => {
-  if (err) throw err;
-
-  readDir(fromDir, 0);
-});
+        if (state.isDirectory()) {
+          readDir(localBase, level + 1);
+        } else {
+          copyFn(item, localBase);
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 const copyFn = (item, itemLink) => {
   const firstSymbol = item.substring(0, 1).toUpperCase();
   const targetDir = path.join(toDir, firstSymbol);
 
-  fs.mkdir(targetDir, { recursive: true }, (err) => {
-    if (err) throw err;
-
-    fs.link(itemLink, path.join(targetDir, item), err => {
-      if (err) {
-        console.error(err.message);
-      }
+  mkdirPromisify(targetDir, { recursive: true })
+    .then(linkPromisify(itemLink, path.join(targetDir, item)))
+    .catch(err => {
+      console.log(err);
     });
-  });
 };
 
-const readDir = (base, level) => {
-  const files = fs.readdirSync(base);
+fs.access(fromDir, err => {
+  if (err) {
+    console.log(`${fromDir} folder does not exist.\n ${err}`);
+    process.exit(9);
+  }
+});
 
-  files.forEach(item => {
-    const localBase = path.join(base, item);
-    const state = fs.statSync(localBase);
-
-    if (state.isDirectory()) {
-      readDir(localBase, level + 1);
-    } else {
-      copyFn(item, localBase);
-    }
+mkdirPromisify(toDir, { recursive: true })
+  .then(readDir(fromDir, 0))
+  .catch(err => {
+    console.log(err);
   });
-};
-
-if (deleteArg) {
-  fs.rmdir(fromDir, { recursive: true }, err => {
-    if (err) {
-      console.error(err.message);
-    }
-  });
-}
